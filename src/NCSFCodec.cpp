5ec37521037c7b2655eadfc391358d7b79a32b7f
Alwin Esch <alwin.esch@web.de>
2020-08-05 18:46:52 +0200

    update psflib to version 591f2a3b31 (25 May 2020)

--- src/NCSFCodec.cpp
+++ src/NCSFCodec.cpp
@@ -19,7 +19,7 @@ extern "C"
 #include <stdint.h>
 #include <stdio.h>
 
-  static void* psf_file_fopen(const char* uri)
+  static void* psf_file_fopen(void* context, const char* uri)
   {
     kodi::vfs::CFile* file = new kodi::vfs::CFile;
     if (!file->OpenFile(uri, 0))
@@ -56,8 +56,9 @@ extern "C"
     return file->GetPosition();
   }
 
-  const psf_file_callbacks psf_file_system = {"\\/",          psf_file_fopen,  psf_file_fread,
-                                              psf_file_fseek, psf_file_fclose, psf_file_ftell};
+  const psf_file_callbacks psf_file_system = {"\\/",          nullptr,        psf_file_fopen,
+                                              psf_file_fread, psf_file_fseek, psf_file_fclose,
+                                              psf_file_ftell};
 
   inline unsigned get_le32(void const* p)
   {
